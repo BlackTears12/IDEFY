472906b258e3aec3d3862cb95f054ccd97057108
BlackTears12 <szombati.balint@gmail.com>
2025-04-29 11:08:35 +0200

    recipefinder implementation added

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include "mainwindow.hpp"
 
 #include <QApplication>
+#include "filematcher.hpp"
 #include "parser.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     yocto::Parser p;
-    p.parseConfigFile("test.yocto");
-    MainWindow w;
+    p.parseConfigFile("/home/balint/yocto-ws/poky/meta-yocto-bsp/conf/layer.conf");
+    auto list = util::FileMatcher::findFilesByPathPattern(
+        "/home/balint/yocto-ws/poky/meta-yocto-bsp/",
+        "/home/balint/yocto-ws/poky/meta-yocto-bsp/recipes-*/*/*.bbapend");
+    for (auto &e : list) {
+        qDebug() << e << "\n";
+    }
+    //MainWindow w;
     //w.show();
     //return a.exec();
 }
